7117146026253621e78fa44c1ff8d46e6995400e
Verisonder <107511835+Verisonder@users.noreply.github.com>
2026-07-07 19:04:23 +0000

    v2.9.0

--- sondercat.py
+++ sondercat.py
@@ -145,7 +145,7 @@ except Exception:
     sys.exit(1)
 
 APP_NAME = "SondeR cat"
-APP_VERSION = "2.8.0"
+APP_VERSION = "2.9.0"
 CONFIG_PATH = os.path.join(os.path.expanduser("~"), ".sondercat.json")
 AGENT_FILE = os.path.join(os.path.expanduser("~"), ".sondercat_agent")
 
